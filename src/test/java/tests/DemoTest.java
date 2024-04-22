6b7544bc173c5f8a7671c3abff9d8cfeeeeaf222
FabianMamani <fabian.mamani@hotmail.com>
2024-04-22 20:51:52 -0300

    update

---
+++ src/test/java/tests/DemoTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DemoTest {
+
+    @Test
+    public void check(){
+        assertEquals(2,2);
+    }
+}
